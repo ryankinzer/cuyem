9c984aa69caa7430f4ed3353200f6f89ddf464f6
Tyler Stright <tylers@nezpercetribe.org>
2022-03-01 08:44:39 -0700

    removed 14 day average and added 7 day average max. added hint for field names.

--- R/sum_WaterTempData.R
+++ R/sum_WaterTempData.R
@@ -22,32 +22,28 @@ sum_WaterTempData <- function(data){
   # daily average by date/location/instrument name
   sum_df <- data %>%
     arrange(readingdatetime) %>%
-    group_by(locationlabel, reading_date, instrument_name) %>% # should this be instrumentid and locationid?
+    group_by(locationlabel, reading_date, instrument_name) %>%
     summarize(daily_mean = mean(watertemperature),
               daily_min = min(watertemperature),
               daily_max = max(watertemperature)) %>%
     ungroup() %>%
     mutate(`7dma` = (1/7)*(daily_mean +
-                           dplyr::lag(daily_mean, n = 6) +
-                           dplyr::lag(daily_mean, n = 5) +
-                           dplyr::lag(daily_mean, n = 4) +
-                           dplyr::lag(daily_mean, n = 3) +
-                           dplyr::lag(daily_mean, n = 2) +
-                           dplyr::lag(daily_mean, n = 1))) %>%
-    mutate(`14dma` = (1/14)*(daily_mean +
-                             dplyr::lag(daily_mean, n = 13) +
-                             dplyr::lag(daily_mean, n = 12) +
-                             dplyr::lag(daily_mean, n = 11) +
-                             dplyr::lag(daily_mean, n = 10) +
-                             dplyr::lag(daily_mean, n = 9) +
-                             dplyr::lag(daily_mean, n = 8) +
-                             dplyr::lag(daily_mean, n = 7) +
                              dplyr::lag(daily_mean, n = 6) +
                              dplyr::lag(daily_mean, n = 5) +
                              dplyr::lag(daily_mean, n = 4) +
                              dplyr::lag(daily_mean, n = 3) +
                              dplyr::lag(daily_mean, n = 2) +
-                             dplyr::lag(daily_mean, n = 1)))
+                             dplyr::lag(daily_mean, n = 1))) %>%
+    mutate(`7dadm` = (1/7)*(daily_max +
+                              dplyr::lag(daily_max, n = 6) +
+                              dplyr::lag(daily_max, n = 5) +
+                              dplyr::lag(daily_max, n = 4) +
+                              dplyr::lag(daily_max, n = 3) +
+                              dplyr::lag(daily_max, n = 2) +
+                              dplyr::lag(daily_max, n = 1)))
+
+  cat('7dma = 7 day moving average',
+      '7dadm = 7 day average daily maximum', sep = '\n')
 
   return(sum_df)
 }
